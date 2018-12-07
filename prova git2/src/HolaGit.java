62b1acbcb260bb6dcc6b68da93bc6babd891c17b
ricardo <ricardosu@gmail.com>
2018-12-07 13:24:08 +0100

    cmabios en master

--- prova git2/src/HolaGit.java
+++ prova git2/src/HolaGit.java
@@ -5,7 +5,6 @@ public class HolaGit {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Adios Mundo");
-
+		System.out.println("A ver que pasa");
 	}
-
 }
